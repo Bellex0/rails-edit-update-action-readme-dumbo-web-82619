49e7c1c042533ccd81264333acf93226e0872c11
Belle P <bellepx3@gmail.com>
2019-09-23 20:58:10 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,6 +18,12 @@ class ArticlesController < ApplicationController
     @article.save
     redirect_to article_path(@article)
   end
+  
+  def edit
+  @article = Article.find(params[:id])
+end
+
+
 
   # add edit and update methods here
 end
